9b9b49ef68791e8911018b0383ce858391bec3d1
原貴悌 <taka_raison@genkiteinoiMac.local>
2021-04-30 13:05:43 +0900

    initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+
+import React from 'react'
+import {Button} from 'react-native'
+
+const CustomButton = ({title, onPress}) =>{
+   return <Button title={title} onPress={()=>  onPress()}/>
+};
+
+export default CustomButton;
